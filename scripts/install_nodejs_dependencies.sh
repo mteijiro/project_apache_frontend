fe43366eb9a861e7602d80e1a5ef44e928615abb
root <root@DESKTOP-QEECBS3.localdomain>
2018-03-27 13:58:02 +0200

    Added AWS setup files

---
+++ scripts/install_nodejs_dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo su
+chown ubuntu:ubuntu /home/ubuntu/www
+npm install
+npm install vue-router
+npm install vue-resource
+npm install forever -g
+
---
+++ scripts/stop_application.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo fuser -k 8080/tcp
