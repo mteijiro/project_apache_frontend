e7e3a7c036194671dc3de2108a6617a4e3edfa48
Michael Taylor <mteijiro@users.noreply.github.com>
2018-03-27 14:37:55 +0200

    Update SubmitAComplaint.vue

--- src/components/SubmitAComplaint.vue
+++ src/components/SubmitAComplaint.vue
@@ -107,7 +107,7 @@ export default {
       complaintForm.append('comments', newComplaint.comments)
       complaintForm.append('image', newComplaint.imageUP, newComplaint.imageUP.name)
       complaintForm.append('audio', newComplaint.audioUP, newComplaint.audioUP.name)
-      fetch('http://localhost:8000/get-token/', {
+      fetch('http://18.197.8.126:8000/get-token/', {
         mode: 'cors',
         body: credentialsForm,
         method: 'POST'
@@ -116,7 +116,7 @@ export default {
         .then(tokenString => 'Token ' + JSON.parse(tokenString)) // Remove string quotations and concatenate with authorization syntax
         .then(resp4 => {
           alert(resp4) // (For debugging purposes) print out the token.
-          fetch('http://localhost:8000/complaints/', {
+          fetch('http://18.197.8.126:8000/complaints/', {
             mode: 'cors',
             headers: {
               'Authorization': resp4
