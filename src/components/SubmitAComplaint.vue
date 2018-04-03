e8a7d5ffbb40fe187bfd8cc85f378c215a396073
root <root@DESKTOP-QEECBS3.localdomain>
2018-04-04 00:59:59 +0200

    fixed backend IP reference

--- src/components/SubmitAComplaint.vue
+++ src/components/SubmitAComplaint.vue
@@ -238,7 +238,7 @@ export default {
     sendToDatabase (myCredentials, newComplaint) {
       const credentialsForm = this.compileCredentials(myCredentials)
       const complaintForm = this.compileComplaint(newComplaint)
-      fetch('http://18.197.8.126:8000/get-token/', {
+      fetch('http://18.197.28.234:8000/get-token/', {
         mode: 'cors',
         body: credentialsForm,
         method: 'POST'
@@ -247,7 +247,7 @@ export default {
         .then(tokenString => 'Token ' + JSON.parse(tokenString)) // Remove string quotations and concatenate with authorization syntax
         .then(resp4 => {
           // alert(resp4) // (For debugging purposes) print out the token.
-          fetch('http://18.197.8.126:8000/complaints/', {
+          fetch('http://18.197.28.234:8000/complaints/', {
             mode: 'cors',
             headers: {
               'Authorization': resp4
