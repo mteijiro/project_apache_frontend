4934657ab92b0b88c4a1bb2b9dfb7c3c98f36df7
Michael Taylor <mttaylor@wpi.edu>
2018-04-04 15:31:14 +0200

    Fix for server not changing directory into the dist folder

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/www/apache_frontend/dist/
+sudo cd /home/ubuntu/www/apache_frontend/dist/
 sudo python3 -m http.server 80 --bind 0.0.0.0
 sleep 3
 sudo fuser -k 80/tcp
