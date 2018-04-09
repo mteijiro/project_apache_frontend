4da9eb3906bd43fa53181b03286dd60f12cc74a7
root <root@DESKTOP-QEECBS3.localdomain>
2018-04-09 16:18:13 +0200

    Updated runserver script and re-ran build

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-sudo cd /home/ubuntu/www/apache_frontend/dist/
+cd /home/ubuntu/www/apache_frontend/dist/
 sudo python3 -m http.server 80 --bind 0.0.0.0
-sleep 3
-sudo fuser -k 80/tcp
