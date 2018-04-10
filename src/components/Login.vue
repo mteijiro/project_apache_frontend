d6366d21c1452be61ae52ca70fa5f8355c9ee8e1
Michael Taylor <mttaylor@wpi.edu>
2018-04-10 14:45:13 +0200

    Add logout button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,18 +4,23 @@
 <template>
   <div>
     <h1>Login</h1>
-    <md-field>
-      <label>Username:</label>
-      <md-input v-model="myCredentials.username"></md-input>
-    </md-field>
-    <md-field>
-      <label>Password:</label>
-      <md-input type="password" v-model="myCredentials.password"></md-input>
-    </md-field>
-    <p>Our website uses cookies to store your authentication details.</p>
-    <md-button class="md-raised md-primary" v-on:click="login()">Submit</md-button>
-    <md-button class="md-raised md-primary" v-on:click="getCookie('token')">TestCookie</md-button>
-    <p id="errorMsg" v-if="invalidToken">Invalid username or password, please try again</p>
+    <template v-if="!loggedIn">
+      <md-field>
+        <label>Username:</label>
+        <md-input v-model="myCredentials.username"></md-input>
+      </md-field>
+      <md-field>
+        <label>Password:</label>
+        <md-input type="password" v-model="myCredentials.password"></md-input>
+      </md-field>
+      <p>Our website uses cookies to store your authentication details.</p>
+      <md-button class="md-raised md-primary" v-on:click="login()">Log In</md-button>
+      <md-button class="md-raised md-primary" v-on:click="getCookie('token')">TestCookie</md-button>
+      <p id="errorMsg" v-if="invalidToken">Invalid username or password, please try again</p>
+    </template>
+    <template v-if="loggedIn">
+      <md-button class="md-raised md-primary" v-on:click="logout()">Log Out</md-button>
+    </template>
   </div>
 </template>
 
@@ -29,7 +34,8 @@ export default {
         password: '',
         token: ''
       },
-      invalidToken: false
+      invalidToken: false,
+      loggedIn: false
     }
   },
   methods: {
@@ -42,14 +48,19 @@ export default {
     },
     login () {
       var credentialsForm = this.compileCredentials(this.myCredentials)
-      fetch('http://18.197.28.234:8000/get-token/', {
+      fetch('http://localhost:8000/get-token/', {
         mode: 'cors',
         body: credentialsForm,
         method: 'POST'
       }).then(this.handleErrors)
         .then(response => response.json()) // Convert the token response into a JSON object
         .then(JSONresponse => JSON.stringify(JSONresponse.token)) // Select the token string from the object.
-        .then(token => { this.myCredentials.token = token; console.log(token); document.cookie = 'username=' + this.myCredentials.username; document.cookie = 'token=' + token })
+        .then(token => {
+          this.myCredentials.token = token
+          console.log(token)
+          document.cookie = 'username=' + this.myCredentials.username
+          document.cookie = 'token=' + token
+        })
         .then(response => {
           console.log('Complaint Success')
           this.invalidToken = false
@@ -59,6 +70,11 @@ export default {
           console.log(error)
           this.invalidToken = true
         })
+    },
+    logout () {
+      this.deleteCookie('username')
+      this.deleteCookie('token')
+      this.loggedIn = false
     },
     getCookie (cname) {
       var name = cname + '='
@@ -76,12 +92,28 @@ export default {
       }
       return ''
     },
+    deleteCookie (name) {
+      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    },
     handleErrors (response) {
       if (!response.ok) {
         throw Error(response.statusText)
       }
       return response
+    },
+    checkForCookies () {
+      // this.getCookie('username')
+      this.myCredentials.token = this.getCookie('token')
+      console.log(this.myCredentials.token)
+      if (this.myCredentials.token.length > 0) {
+        return true
+      } else {
+        return false
+      }
     }
+  },
+  mounted () {
+    this.loggedIn = this.checkForCookies()
   }
 }
 </script>
